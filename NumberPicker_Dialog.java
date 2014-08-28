60bd4e6d6709915416edbfa76b26fbf83fd53e5b
Dario Henry <dariohen@hotmail.com>
2014-08-28 01:49:27 -0500

    Code for NumberPicker Dialog
    Shows method to build the dialog and method to display it.

---
+++ NumberPicker_Dialog.java
@@ -0,0 +1,72 @@
+public void showVolumeDialog(View view) 
+{
+        //Create bundle set initial values and limits
+        Bundle bundle = new Bundle();
+        bundle.putInt("min", 1);
+        bundle.putInt("max", 50);
+        bundle.putInt("current", 1);
+
+        //Instantiate NumberPicker Fragment
+        NumberPickerDialogFragment volFragment = new NumberPickerDialogFragment();
+
+        //set bundle to fragment
+        volFragment.setArguments(bundle);
+
+        //Inflate dialog
+        volFragment.show(getSupportFragmentManager(),"volumePicker");
+}
+
+public static class NumberPickerDialogFragment extends DialogFragment implements NumberPicker.OnValueChangeListener
+{
+    private  int _min, _max, _current;
+    private  NumberPicker.OnValueChangeListener _listener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState)  {
+
+        //get values stored in bundle
+        savedInstanceState = this.getArguments();
+
+        //set variables to bundle values
+        _min = savedInstanceState.getInt("min");
+        _max = savedInstanceState.getInt("max");
+        _current = savedInstanceState.getInt("current");
+        _listener = this;
+
+        //must have a xml file with a numberPicker element
+        LayoutInflater inflater = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.number_picker, null);
+
+        //NumberPicker set up
+        NumberPicker numberPicker = (NumberPicker) view.findViewById(R.id.numberPicker);
+        numberPicker.setMaxValue(_max);
+        numberPicker.setMinValue(_min);
+        numberPicker.setValue(_current);
+        numberPicker.setOnValueChangedListener(_listener);
+
+        //Set up AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.dialog_title);
+        builder.setView(view);
+        builder.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                //Cancel dialog
+                NumberPickerDialogFragment.this.getDialog().cancel();
+            }
+        });
+        builder.setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                //Display current number as a test
+                Toast.makeText(getActivity().getApplicationContext(), String.valueOf(_current),Toast.LENGTH_SHORT).show();
+            }
+        });
+        return builder.create();
+    }
+
+    @Override
+    public void onValueChange(NumberPicker numberPicker, int oldVal, int newVal) {
+        _current = newVal;
+    }
+}
